ceb31e6109a870c7c29e2a54d8711f1c129bc022
Sakshi <sakshishetty1231997@gmail.com>
2021-09-25 23:53:34 +0530

    Added: Test case to calculate fare based on less distance and less time and to return minimum fair

--- src/test/java/com/bridgelabz/InvoiceServiceTest.java
+++ src/test/java/com/bridgelabz/InvoiceServiceTest.java
@@ -22,4 +22,13 @@ public class InvoiceServiceTest {
         double fare = invoiceGenerator.calculateFare(distance, time);
         Assertions.assertEquals(15, fare);
     }
+
+    @Test
+    public void givenLessDistanceAndLessTime_ShouldReturnTotalFare() {
+        InvoiceGenerator invoiceGenerator = new InvoiceGenerator();
+        double distance = 0.1;
+        int time = 1;
+        double fare = invoiceGenerator.calculateFare(distance, time);
+        Assertions.assertEquals(5, fare);
+    }
 }
